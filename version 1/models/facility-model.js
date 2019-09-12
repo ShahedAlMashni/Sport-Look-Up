85e91e7f0c1118bdea2c87ec7c7e34dc5a01a941
ShahedAlMashni <41443850+ShahedAlMashni@users.noreply.github.com>
2019-09-12 18:50:36 +0300

    Update facility-model.js

--- version 1/models/facility-model.js
+++ version 1/models/facility-model.js
@@ -23,25 +23,3 @@ var facilitySchema=   new mongoose.Schema({
 });
 var Campground= mongoose.model('Facility',facilitySchema);
 module.exports=Campground;
-// var campgrounds=[
-//     {name: "salmon Creek", image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b014459df1c979a2e9b5_340.jpg"},
-//     {name: "hello ", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f8c071a7e8b1b8_340.jpg"},
-//     {name: "hello ", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f8c071a7e8b1b8_340.jpg"},
-//
-// Campground.create(
-// {
-//     name: "Wadi rum",
-//     image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b014459df1c979a2e9b5_340.jpg",
-//     description: "This is a huge desert camp,no bathrooms. No water. Beautiful scenes"
-//     price: 5JD
-//
-// },
-// function (err,campground) {
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log(campground);
-//     }
-// }
-// );
